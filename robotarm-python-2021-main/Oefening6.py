85c334a73639f9c6c90a3d0ef4e2ffaa5043b933
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-09-30 14:08:20 +0200

    Oefening6

---
+++ robotarm-python-2021-main/Oefening6.py
@@ -0,0 +1,20 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 6')
+
+# Jouw python instructies zet je vanaf hier:
+for i in range(7):
+    robotArm.moveRight()
+
+for i in range(8):
+    robotArm.grab()
+    
+    robotArm.moveRight()
+
+    robotArm.drop()
+    
+    for i in range(2):
+        robotArm.moveLeft()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
