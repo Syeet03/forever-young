f316019404db2c58b4f05f4a3fca0e5b7fb3af57
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-10-27 09:34:52 +0200

    Oefening 12

--- robotarm-python-2021-main/Oefening12.py
+++ robotarm-python-2021-main/Oefening12.py
@@ -5,13 +5,17 @@ robotArm = RobotArm('exercise 12')
 # Jouw python instructies zet je vanaf hier:
 for i in range(0,8):
     robotArm.moveRight()
+
 for i in range(1,9):
     robotArm.grab()
     scan = robotArm.scan()
+
     if scan == "red":
         for c in range(0,i):
             robotArm.moveRight()
-        robotArm.drop()  
+            
+        robotArm.drop() 
+
         for c in range(0,i):
             robotArm.moveLeft()  
     else:
